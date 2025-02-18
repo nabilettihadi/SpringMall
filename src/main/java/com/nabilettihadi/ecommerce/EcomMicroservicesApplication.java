b669ccd8ddf160173e36dfb8f274ccd8ec89ac8f
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-02-18 15:02:29 +0100

    Initialisation de projet

---
+++ src/main/java/com/nabilettihadi/ecommerce/EcomMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.nabilettihadi.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomMicroservicesApplication.class, args);
+	}
+
+}
